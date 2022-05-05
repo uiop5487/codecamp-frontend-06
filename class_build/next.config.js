d990fc7c210ec7901f68d3c2dc78dda1f63f7fdc
uiop5487 <uiop5487@gmail.com>
2022-05-05 13:27:11 +0900

    도커 분기

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  /** 아이디를 통일 시켜줌 */
+  generateBuildId: () => "tthh12",
+  /** 정적파일을 지정해 줌 */
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
